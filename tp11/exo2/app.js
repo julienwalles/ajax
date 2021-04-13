de3fb2ac1e22cc14d9b059610022a22ee9abcfb8
Julien Walles <j.walles@cci-formation.nc>
2021-04-14 10:31:08 +1100

    exo2

--- tp11/exo2/app.js
+++ tp11/exo2/app.js
@@ -7,11 +7,19 @@ function loadDoc() {
     xhttp.onreadystatechange = function () {
 
         if (this.readyState === 4) {
-            
-                document.getElementById("rec").innerText = this.responseText;          
+            var rec = document.getElementById("rec");
+            var data = JSON.parse(this.responseText);
+            var newLi = document.createElement("li");
+            var img = document.createElement("img");
+            var ph1 = "A" + " " + textSaisi +" " + "la T° est de: "
+            var temp = data.main.temp; 
+            img.src = data.weather[0].icon;     
+            newLi.append( ph1 + temp + img.src);
+            rec.appendChild(newLi);         
         }
     };
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="+textSaisi+"&appid=7602a2e8aac0c505880bbdac3714ce84";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+textSaisi+"&appid=7602a2e8aac0c505880bbdac3714ce84"+"&units=metric";
     xhttp.open("GET",url, true);
     xhttp.send();
 }
+
